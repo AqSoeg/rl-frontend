e5aecf57b7b7052cd74e730269c7cfae7fb92131
Xuan-Van <wuqi7137@qq.com>
2024-11-28 21:04:49 +0800

    马上开始状态向量

--- src/components/AgentEditor/ActionSpace.jsx
+++ src/components/AgentEditor/ActionSpace.jsx
@@ -1,7 +1,16 @@
+import actionLogo from '../../assets/actionSpace.svg';
+import uploadLogo from '../../assets/upload.svg';
+
 const ActionSpace = () => {
     return (
         <div className="sub-component">
-            <div className="font-bold mb-2">动作空间</div>
+            <div className="sub-component-banner">
+                <img src={actionLogo} alt = "ActionSpace" className="sub-component-logo" />
+                <div className="sub-component-title">动作空间</div>
+            </div>
+            <div className="upload-button">
+                <img src={uploadLogo} alt = "Upload" className="upload-button-logo" />
+            </div>
         </div>
     );
 };
--- src/components/AgentEditor/RewardFunction.jsx
+++ src/components/AgentEditor/RewardFunction.jsx
@@ -1,7 +1,16 @@
+import rewardLogo from '../../assets/rewardFunction.svg';
+import uploadLogo from '../../assets/upload.svg';
+
 const RewardFunction = () => {
     return (
         <div className="sub-component">
-            <div className="font-bold mb-2">奖赏函数</div>
+            <div className="sub-component-banner">
+                <img src={rewardLogo} alt = "RewardFunction" className="sub-component-logo" />
+                <div className="sub-component-title">奖赏函数</div>
+            </div>
+            <div className="upload-button">
+                <img src={uploadLogo} alt = "Upload" className="upload-button-logo" />
+            </div>
         </div>
     );
 };
