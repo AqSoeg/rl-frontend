61299b26133a0d25aa3ce81bb85b4874b6b0ce20
wf123525 <2303143082@qq.com>
2024-12-13 14:17:52 +0800

    训练服务

--- src/components/TrainingService/TrainingService.jsx
+++ src/components/TrainingService/TrainingService.jsx
@@ -1,16 +1,29 @@
-import React from 'react';
+import React ,{useState, useEffect} from 'react';
 import Left from './left';
 import Middle from './middle'
+import axios from 'axios';
 import Right from './right'
 import './TrainingService.css'; // 引入CSS文件
+const TrainingService = () => {  
+  const [scenarios, setScenarios] = useState([]);
 
+  // 获取场景数据
+  useEffect(() => {
+      const fetchScenarios = async () => {
+          try {
+              const response = await axios.get('http://localhost:3000/scenarios');
+              setScenarios(response.data);
+          } catch (error) {
+              console.error('Error fetching scenarios:', error);
+          }
+      };
 
-const TrainingService = () => {
-
+      fetchScenarios();
+  }, []);
   return (
     <div className='trainingservice'>
         <div className='left'>
-            <Left />
+            <Left scenarios={scenarios}/>
         </div>
         <div className='middle'>
             <Middle />
